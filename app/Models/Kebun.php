8571beda5216965e092c73f77cbebd18d071e537
Lokacodes <talloka21.mhs@akb.ac.id>
2023-07-17 11:28:12 +0700

    replacing fillable with guarded and adding idalat method

--- app/Models/Kebun.php
+++ app/Models/Kebun.php
@@ -10,10 +10,9 @@ class Kebun extends Model
     use HasFactory;
 
     protected $table = 'kebuns';
-    protected $fillable = [
-        'nama_kebun',
-        'lokasi_kebun',
-        'id_user'
+    protected $guarded = [
+        'id',
+        'timestamps'
     ];
 
     //get all data from table which id_user corresponds with the specified id_user
@@ -21,6 +20,10 @@ class Kebun extends Model
     public function tertentu()
     {
         return $this->where('id_user', auth()->user()->id)->get();
+    }
+
+    public function IDAlat(String $id_alat){
+        return $this->where('id_alat', $id_alat)->first();
     }
 
 
